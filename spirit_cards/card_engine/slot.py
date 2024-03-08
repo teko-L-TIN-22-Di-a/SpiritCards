e5072d5149b31a8b3fa8cbf4078646b925105e86
TheCodeWizard27 <benny.schaerer@gmail.com>
2024-03-08 17:56:14 +0100

    Kinda unecessarily complicated and ugly way to fix a small issue.

--- spirit_cards/card_engine/slot.py
+++ spirit_cards/card_engine/slot.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
 from spirit_cards.card_engine.card import Card
 
+_current_slot_id = 0
+
 @dataclass
 class Slot:
+
+    _slot_id: int = 0
 
     HAND_SLOT = "hand"
     SUPPORT_SLOT = "support"
@@ -20,6 +24,19 @@ class Slot:
     attacking: bool = False
     blocked: bool = False
     exhausted: bool = False
+
+    def __init__(
+            self,
+            type: str = None,
+            card: Card = None):
+        global _current_slot_id
+        _current_slot_id += 1
+        self._slot_id = _current_slot_id
+        self.type = type
+        self.card = card
+
+    def __eq__(self, other):
+        return self._slot_id == other._slot_id
 
     def reset(self):
         self.just_summoned = False
